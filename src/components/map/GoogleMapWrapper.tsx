ff71919dcfa6755483bb017f648fd0d0a6619bf6
Zul Ikram Musaddik Rayat <rayathossain49@gmail.com>
2025-11-28 20:55:57 +0600

    feat: update PopoverContent styles to improve usability with max height and overflow

--- src/components/map/GoogleMapWrapper.tsx
+++ src/components/map/GoogleMapWrapper.tsx
@@ -276,7 +276,7 @@ export function GoogleMapWrapper({
           }}
         />
         <PopoverContent
-          className="w-80"
+          className="w-80 max-h-[80vh] overflow-y-auto"
           side="right"
           align="start"
           sideOffset={10}
